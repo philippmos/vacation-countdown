0005f8a9f1f0293ee5333cb4c5f796b323e1caa5
Philipp Mos. <46461566+philippmos@users.noreply.github.com>
2025-06-09 22:53:26 +0200

    Add root outputs

---
+++ infrastructure/outputs.tf
@@ -0,0 +1,11 @@
+output "resource_group_id" {
+  value = module.resource_group.id
+}
+
+output "vacation_site_id" {
+  value = module.vacation_static_site.static_site_id
+}
+
+output "retirement_site_id" {
+  value = module.retirement_static_site.static_site_id
+}
